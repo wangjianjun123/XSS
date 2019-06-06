0d16513b608dd3451e7de866acd55893dfda51ef
wangjj <wangjj@mapbar.com>
2019-06-06 17:04:55 +0800

    firstCommit

--- app/src/main/java/com/kele/xss/MainActivity.kt
+++ app/src/main/java/com/kele/xss/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.kele.xss
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnRecord.setOnClickListener {
+            Toast.makeText(this@MainActivity, "成功", Toast.LENGTH_SHORT).show()
+        }
     }
 }
